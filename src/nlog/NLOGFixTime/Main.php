b9216db7dfb553aa66da6178f259ff1c056b3056
NLOG <sorisem4106@naver.com>
2017-07-15 16:14:23 +0900

    Update Main.php

--- src/nlog/NLOGFixTime/Main.php
+++ src/nlog/NLOGFixTime/Main.php
@@ -7,7 +7,7 @@ use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener{
 
-	 public function onEnable() {
+	 public function onLoad() {
 		date_default_timezone_set("Asia/Seoul");
 		$this->getLogger()->alert("콘솔의 시간대를 UTC +9(서울)로 지정하였습니다.");
 	 }
